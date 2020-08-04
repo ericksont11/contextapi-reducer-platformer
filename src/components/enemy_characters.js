97b00e799618583f6ee77bca6dedd91365c9ef55
Tom Erickson <ericksont91@gmail.com>
2020-08-04 18:41:42 -0400

    changed faces

--- src/components/enemy_characters.js
+++ src/components/enemy_characters.js
@@ -56,7 +56,7 @@ const EnemyCharacters = () => {
       <div className="Enemy-list">
         {enemies.enemyAttributes.map(enemies => {
             return ( 
-                <div className={enemies.name} key={enemies.name} style={{position:'absolute', textAlign:'center', lineHeight:enemies.height, color: 'white', fontSize: '2rem', height:enemies.height, width: enemies.width, backgroundColor:'gray', top:enemies.top, left:enemies.left}}>{enemies.name}</div>
+                <div className={enemies.name} key={enemies.name} style={{position:'absolute', textAlign:'center', lineHeight:enemies.height, color: 'white', fontSize: '1vw', height:enemies.height, width: enemies.width, backgroundColor:'gray', top:enemies.top, left:enemies.left}}>{enemies.name}</div>
             )
           })}
       </div>
